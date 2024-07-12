0c641b0bf8c5726e0dcfc240bc166fa5709cfe84
fullstackn <fullstackn@outlook.com>
2024-07-13 00:30:55 +0300

    initial

---
+++ README.md
@@ -0,0 +1,7 @@
+Script read articles from https://pikabu.ru/ , filtered by tags
+
+For installing dependencies run `pip install -r requirements.txt
+`
+
+Example of usage from command line:
+`python pikabu.py --output_filename my_articles_filename.csv --tags Politics,Mars`
---
+++ pikabu.py
@@ -0,0 +1,87 @@
+import argparse
+import csv
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup, Tag
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+TAGS = 'Politics,Mars'
+OUTPUT_FILENAME = 'out.csv'
+
+
+def get_tag_html(tag: Tag):
+    return ''.join([i.decode() if type(i) is Tag else i for i in tag.contents])
+
+
+def wait_for_page_load(driver, timeout=30):
+    WebDriverWait(driver, timeout).until(
+        lambda driver: driver.execute_script("return document.readyState") == "complete"
+    )
+
+
+def get_stories(page_url):
+    options = Options()
+    options.add_argument("--headless")  # Run in headless mode
+    driver = webdriver.Chrome(options=options)
+    driver.get(page_url)
+    all_stories = []
+    # full load of all stories (through scrolling)
+    while True:
+        wait_for_page_load(driver)
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "stories-feed__message")))
+        all_stories += BeautifulSoup(driver.page_source, 'html.parser').find_all(name='article')
+        if element.get_attribute('style') == 'display: block;':
+            break
+    # filter out duplicates
+    all_stories_filtered = []
+    for st in all_stories:
+        if st.get('data-story-id') not in [s.get('data-story-id') for s in all_stories_filtered]:
+            all_stories_filtered.append(st)
+    return all_stories_filtered
+
+
+def process_stories(story_list):
+    story_data = []
+    for story in story_list:
+        title_tag = story.find(class_='story__title')
+        if title_tag is not None:
+            title = title_tag.find(name='a').text
+        else:
+            title = ''
+        story_tags = [t.attrs['data-tag'] for t in story.find(class_='story__tags').find_all(name='a')]
+        text_tag = story.find(class_='story-block_type_text')
+        if text_tag is not None:
+            text = story.find(class_='story-block_type_text').text
+        else:
+            text = ''
+        story_data.append({
+            'title': title,
+            'text': text,
+            'tags': story_tags,
+        })
+    return story_data
+
+
+if __name__ == '__main__':
+    # get params
+    parser = argparse.ArgumentParser(description='parameters for creating data')
+    parser.add_argument('--output_filename', type=str)
+    parser.add_argument('--tags', type=str)
+    args = parser.parse_args()
+    output_filename = args.output_filename or OUTPUT_FILENAME
+    tags = args.tags or TAGS
+
+    # read Tag list from site
+    stories = get_stories(f'https://pikabu.ru/tag/{tags}')
+    # process this list to get json
+    processed = process_stories(stories)
+    # write result to csv
+    with open(output_filename, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['title', 'text', 'tags'])
+        for item in processed:
+            writer.writerow([item.get('title'), item.get('text'), item['tags']])
